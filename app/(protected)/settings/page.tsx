9a4c766a092e84a328ccd510e3e2c2999d0eb479
Mykhailo Zykov <mykhailozykov@gmail.com>
2024-10-24 16:34:20 +0300

    settings page navbar buttons added

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,23 +1,23 @@
 "use client";
 
 import { logout } from "@/actions/logout";
-import { useCurrentUser } from "@/hooks/use-current-user";
+// import { useCurrentUser } from "@/hooks/use-current-user";
 
 
 const SettingsPagediv = () => {
-  const user = useCurrentUser();
+  // const user = useCurrentUser();
 
   const onClick = () => {
     logout();
   };
   return (
-    <div>
-      {JSON.stringify(user)}
-      <div>
+    <div className="bg-white p-10 rounded-lg">
+      {/* {JSON.stringify(user)} */}
+      
         <button onClick={onClick} type="submit">
           Sign out
         </button>
-      </div>
+      
     </div>
   );
 };
